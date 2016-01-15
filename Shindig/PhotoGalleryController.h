e3ddb174598ede2ed93c4629abeb25c0f0d0ae0d
kthota77 <kamal.thota@xelits.com>
2016-01-15 09:45:26 -0700

    Added PhotoGalleryController and UISearchBar and delegate to view

---
+++ Shindig/PhotoGalleryController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoGalleryController.h
+//  Shindig
+//
+//  Created by Kamalakar Thota on 1/15/16.
+//  Copyright © 2016 KamalFlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoGalleryController : UITableViewController <UISearchBarDelegate>
+
+@end
