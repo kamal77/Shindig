e2f3c7eb08783b6e1d4bb9be96c53c48efb5bb89
kthota77 <kamal.thota@xelits.com>
2016-01-15 15:40:31 -0700

    Made it auto adaptive layout. Downloads are going through the main queue.

--- Shindig/PhotoGalleryController.m
+++ Shindig/PhotoGalleryController.m
@@ -36,11 +36,13 @@
 
 -(void) finishedDownloadingFlickrPhoto:(NSMutableArray *)flickrphotos {
     
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^{
+   // dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(dispatch_get_main_queue(), ^{
         self.photos = flickrphotos;
         [self.tableView reloadData];
     });
+    
+    
    
 }
 
