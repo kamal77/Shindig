d2c13e5e61fc378b88100d2ffd21945bb8523e56
kthota77 <kamal.thota@xelits.com>
2016-01-15 09:25:38 -0700

    Initial Project Commit

---
+++ Shindig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shindig
+//
+//  Created by Kamalakar Thota on 1/15/16.
+//  Copyright © 2016 KamalFlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shindig/PhotosViewControllerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewControllerTableViewController.h
+//  Shindig
+//
+//  Created by Kamalakar Thota on 1/15/16.
+//  Copyright © 2016 KamalFlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewControllerTableViewController : UITableViewController
+
+@end
